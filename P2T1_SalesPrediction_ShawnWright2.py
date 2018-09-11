f6febf46aae51b99c5af312768ed87019312992c
Wright2533 <43157100+Wright2533@users.noreply.github.com>
2018-09-10 20:48:50 -0400

    Add files via upload
    First submission

---
+++ P2T1_SalesPrediction_ShawnWright2.py
@@ -0,0 +1,13 @@
+# Sales Predictions
+# 9/9/18
+# CTI-110 P2T1 - Sales Prediction
+# Shawn Wright
+#
+
+projectedTotalSales = float( input("Please enter the projected amount of total sales: " )  )
+
+
+profit = 0.23 * projectedTotalSales
+
+print( format( profit , ",.2f" ) )
+
