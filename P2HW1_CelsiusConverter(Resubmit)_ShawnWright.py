59f60c7730e16d08833acd2125cb112b1b5b0541
Wright2533 <43157100+Wright2533@users.noreply.github.com>
2018-10-15 14:22:59 -0400

    Add files via upload

---
+++ P2HW1_CelsiusConverter(Resubmit)_ShawnWright.py
@@ -0,0 +1,21 @@
+# Celsius Fahrenheit Converter  
+# 9/10/18
+# CTI-110 P2HW1 - Celsius Fahrenheit Converter 
+# Shawn Wright
+#
+
+#temperature in celcius c= celcius
+
+c = float(input('Enter temperature in celsius: '))
+
+#convert farenheit F = Fahrenheit FE = 9/5c
+
+FE = c* 1.8
+F = FE + 32
+
+#show the conerted temperature in Farenheit
+
+print('temperature in Farenheit is', F)
+
+    
+
